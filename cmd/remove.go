74275e989ce1aa423665aa0e87a5a0558f289c83
Atsushi Toda <todaatsushi0@gmail.com>
2022-05-07 19:04:10 +0100

    feat (remove): remove tag functionality

--- cmd/remove.go
+++ cmd/remove.go
@@ -6,6 +6,7 @@ package cmd
 
 import (
 	"fmt"
+	"internal/repoconf"
 
 	"github.com/spf13/cobra"
 )
@@ -14,20 +15,38 @@ var removeCmd = &cobra.Command{
 	Use:   "remove",
 	Short: "Remove items or tags",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("remove called")
+		tagCmd, _ := cmd.Flags().GetBool("tag")
+		if tagCmd == true {
+			removeTag(cmd, args)
+		} else {
+			fmt.Println("remove item")
+		}
 	},
+}
+
+func removeTag(cmd *cobra.Command, tags []string) {
+	config := repoconf.ReadConfig()
+	toRemove := make(map[string]bool)
+	currentTags := config.Tags
+	for _, tag := range tags {
+		toRemove[tag] = true
+	}
+
+	newTags := make([]string, 0)
+	for _, tag := range currentTags {
+		if _, ok := toRemove[tag]; !ok {
+			newTags = append(newTags, tag)
+		}
+	}
+	config.Tags = newTags
+	repoconf.WriteConfig(config)
+
+	fmt.Println("Tags successfully removed.")
 }
 
 func init() {
 	rootCmd.AddCommand(removeCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// removeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// removeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	flags := removeCmd.Flags()
+	flags.BoolP("tag", "t", false, "Indicates that you want to add a tag, not a resource")
 }
