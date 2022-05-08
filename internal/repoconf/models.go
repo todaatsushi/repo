8d0f5a2b68a291312cf8ec7589a32e7c1f1566d0
Atsushi Toda <todaatsushi0@gmail.com>
2022-05-08 16:38:25 +0100

    feat (models): define json data schema

--- internal/repoconf/models.go
+++ internal/repoconf/models.go
@@ -1,5 +1,20 @@
 package repoconf
 
+type Memo struct {
+	Tags        []string `json:"tags"`
+	Content     string   `json:"content"`
+	Description string   `json:"description"`
+}
+
 type Config struct {
-	Dirs []string `json:"dirs"`
+	Tags  []string        `json:"tags"`
+	Items map[string]Memo `json:"items,omitempty"`
+}
+
+func NewMemo(content string) Memo {
+	return Memo{Content: content}
+}
+
+func NewConfig() Config {
+	return Config{}
 }
