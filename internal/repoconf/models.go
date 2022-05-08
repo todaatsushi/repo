43638dd3a42c30a49a3e7176b4c7dcbd08cdb4db
Atsushi Toda <todaatsushi0@gmail.com>
2022-05-08 16:42:51 +0100

    feat (models): add name field to reference items

--- internal/repoconf/models.go
+++ internal/repoconf/models.go
@@ -4,6 +4,7 @@ type Memo struct {
 	Tags        []string `json:"tags"`
 	Content     string   `json:"content"`
 	Description string   `json:"description"`
+	Name        string   `json:"name"`
 }
 
 type Config struct {
@@ -11,8 +12,8 @@ type Config struct {
 	Items map[string]Memo `json:"items"`
 }
 
-func NewMemo(content string) Memo {
-	return Memo{Content: content}
+func NewMemo(content string, name string) Memo {
+	return Memo{Content: content, Name: name}
 }
 
 func NewConfig() Config {
